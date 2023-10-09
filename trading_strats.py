5c299a97e3a3e499710a2199333a0657d31dbfbe
antoine chambellan <antoine.chambellan2@gmail.com>
2023-10-09 18:31:31 +0200

    Strats --> 12

--- trading_strats.py
+++ trading_strats.py
@@ -5,10 +5,11 @@ Created on Thu Sep 28 17:20:17 2023
 @author: antch
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def covered_call(S0, S, K, p):
-    return K - S0 - np.max(S-K,0) + p
+    return K - S0 - np.max(K-S,0) + p
 
 def covered_put(S0, S, K, p):
     return S0 - S - np.max(K-S,0) + p
@@ -24,8 +25,30 @@ def bull_call_spread(S0, S, K1, K2, p1, p2):
     return np.max(S-K1,0) - np.max(S-K2,0) - p1 + p2
 
 def bull_put_spread(S0, S, K1, K2, p1, p2):
-    #K1 < K2  and p2 > p1
     return np.max(K1-S, 0) - np.max(K2 - S,0) - p1 + p2
 
-def test(r):
-    return r*90
+def bear_call_spread(S0, S, K1, K2, p1, p2):
+    return np.max(S-K1,0) - np.max(S-K2,0) + p2 - p1
+
+def bear_put_spread(S0, S, K1, K2, p1, p2):
+    #K2 < K1
+    return np.max(K1-S,0) - np.max(K2-S,0) + p2 - p1
+
+def long_synthetic_forward(S, K, p1, p2):
+    return np.max(S-K,0) - np.max(K-S,0) + p2 - p1
+
+def short_synthetic_forward(S, K, p1, p2):
+    return np.max(K-S,0) - np.max(S-K,0) + p2 - p1
+
+def long_combo(S, K1, K2, p1, p2):
+    return np.max(S-K1,0) - np.max(K2-S,0) + p2 - p1
+
+def short_combo(S, K1, K2, p1, p2):
+    #K1 < K2
+   return np.max(K1-S,0) - np.max(S - K2, 0) + p2 - p1
+
+def bull_call_ladder(S0, S, K1, K2, K3, p1, p2, p3):
+    return S0 + np.max(S-K1,0) - np.max(S-K2,0) - np.max(S-K3,0) + p2 + p3 - p1
+
+
+
